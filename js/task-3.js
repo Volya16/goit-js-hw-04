ef6bee50f9758c56cf0c19899eae138103f34003
Olya Vorontsova <16olya1996@gmail.com>
2024-11-15 19:41:24 +0100

    add third task

---
+++ js/task-3.js
@@ -0,0 +1,24 @@
+const profile = {
+  username: "Jacob",
+    playTime: 300,
+    getInfo() {
+      return `${this.username} has ${this.playTime} active hours!`;
+    },
+    updatePlayTime(hours) {
+        this.playTime = Number(this.playTime) + Number(hours);
+    },
+    changeUsername(newName) {
+        this.username = newName;
+    }
+
+};
+
+
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
